dc3ecf27e941be7e9529edb5bc123e49605b3d3c
mastery-dhananjay-jadhav <ext.dhananjay.jadhav@mastery.net>
2021-04-15 14:51:34 +0530

    Typeorm Configuration

---
+++ src/task/task.entity.ts
@@ -0,0 +1,18 @@
+import { TaskStatus } from 'src/task/task.model';
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Task extends BaseEntity {
+
+    @PrimaryGeneratedColumn()
+    id:number;
+
+    @Column()
+    title:string;
+
+    @Column()
+    description:string;
+
+    @Column()
+    status: TaskStatus;
+}
