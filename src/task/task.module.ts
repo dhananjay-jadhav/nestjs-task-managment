e730e7353092861e55600449f9625409a61485f9
mastery-dhananjay-jadhav <ext.dhananjay.jadhav@mastery.net>
2021-04-15 15:10:36 +0530

    created task repository

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,8 +1,13 @@
+import { TaskRepository } from './task.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { TaskController } from './task.controller';
 import { TaskService } from './task.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([TaskRepository])
+  ],
   controllers: [TaskController],
   providers: [TaskService],
 })
