cd7f98946e607e49339c1bfbec2876f58182369c
mastery-dhananjay-jadhav <ext.dhananjay.jadhav@mastery.net>
2021-04-17 20:53:05 +0530

    updated class validator

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,9 +1,14 @@
-import { IsNotEmpty } from "class-validator";
+import { IsNotEmpty,IsString, Matches, MaxLength, minLength, MinLength } from "class-validator";
 
 export class AuthCredentialsDto {
-    @IsNotEmpty()
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
     fullName:string;
     
-    @IsNotEmpty()
+    @IsString()
+    @MinLength(8)
+    @MaxLength(20)
+    @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/)
     password:string;
 }
