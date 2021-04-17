a450bfb43bd487483d1f66c2c10ebee9cf155bc1
mastery-dhananjay-jadhav <ext.dhananjay.jadhav@mastery.net>
2021-04-17 21:05:40 +0530

    add message in Matches decorator

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,14 +1,22 @@
-import { IsNotEmpty,IsString, Matches, MaxLength, minLength, MinLength } from "class-validator";
+import {
+  IsString,
+  Matches,
+  MaxLength,
+  minLength,
+  MinLength,
+} from 'class-validator';
 
 export class AuthCredentialsDto {
-    @IsString()
-    @MinLength(4)
-    @MaxLength(20)
-    fullName:string;
-    
-    @IsString()
-    @MinLength(8)
-    @MaxLength(20)
-    @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/)
-    password:string;
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  fullName: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password is too weak',
+  })
+  password: string;
 }
