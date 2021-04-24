13a400e33eef85b3209c54067156a43a90689e6e
mastery-dhananjay-jadhav <ext.dhananjay.jadhav@mastery.net>
2021-04-24 16:40:43 +0530

    singing a jwt token upon authonticatioon

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -14,12 +14,14 @@ export class AuthController {
 
   @Post('/signup')
   @UsePipes(ValidationPipe)
-  signUp(@Body() authCredentialsDto: AuthCredentialsDto):Promise<void> {
+  signUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.authService.signUp(authCredentialsDto);
   }
 
   @Post('/signin')
-  signIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto):Promise<string> {
+  signIn(
+    @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
     return this.authService.signIn(authCredentialsDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,36 @@
+import { JwtPayLoad } from './jwt-payload.interface';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UserRepository } from './user.repository';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
     private userRepository: UserRepository,
+    private jwtServide: JwtService,
   ) {}
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.userRepository.signUp(authCredentialsDto);
   }
 
-  async signIn(authCredentialsDto: AuthCredentialsDto):Promise<string>{
-    const userName = await this.userRepository.validateUserPassword(authCredentialsDto);
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    const userName = await this.userRepository.validateUserPassword(
+      authCredentialsDto,
+    );
 
-    if(!userName){
+    if (!userName) {
       throw new UnauthorizedException('username or password is incorrect');
     }
 
-    return userName;
+    const payload: JwtPayLoad = { userName };
+    const accessToken = await this.jwtServide.sign(payload);
+
+    return { accessToken };
   }
 }
